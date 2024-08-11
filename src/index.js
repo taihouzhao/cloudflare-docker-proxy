48eddc83de61a23bc766caf71ce67fdfa424d90c
Xingzhong <117575604+taihouzhao@users.noreply.github.com>
2024-08-11 17:36:33 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.dashjie.net": dockerHub,
+  "quay.dashjie.net": "https://quay.io",
+  "gcr.dashjie.net": "https://gcr.io",
+  "k8s-gcr.dashjie.net": "https://k8s.gcr.io",
+  "k8s.dashjie.net": "https://registry.k8s.io",
+  "ghcr.dashjie.net": "https://ghcr.io",
+  "cloudsmith.dashjie.net": "https://docker.cloudsmith.io",
+  "ecr.dashjie.net": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.dashjie.net": dockerHub,
 };
 
 function routeByHosts(host) {
